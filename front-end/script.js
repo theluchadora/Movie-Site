d0981646875c5280fa50c8c52382e002dcd75888
theluchadora <160585859+theluchadora@users.noreply.github.com>
2025-05-05 23:51:48 +0300

    editing api link

--- front-end/script.js
+++ front-end/script.js
@@ -1,6 +1,6 @@
-const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=be4498117880e61f5ce816cf0c2f0489&page=1';
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=yourapi&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=be4498117880e61f5ce816cf0c2f0489&query=";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=yourapi&query=";
 
 
 const main = document.getElementById("section");
